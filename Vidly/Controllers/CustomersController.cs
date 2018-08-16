d24c4f5a7d8d49b639c44a2313978a189a5916ad
jackhc <34367064+jackhhchan@users.noreply.github.com>
2018-08-16 10:18:28 +1000

    Vidly
    Commit from onedrive folder

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        /* ================ Database Object ================*/
+
+        // Create Database object a class attribute
+        private MyDbContext _context;
+        // Define the _context object
+        public CustomersController()
+        {
+            _context = new MyDbContext();
+        }
+
+        // Create , Override Dispose method for this database object.
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
+        /* ============== Actions ================*/
+        // GET: Customers
+        [Route("customers")]
+        public ViewResult Index()
+        {
+            //var customers = GetCustomers().ToList();
+
+            var customers = _context.Customers.Include(c=>c.MembershipType).ToList(); //Obtain customer list from DB.
+            var indexcustomerviewmodel = new IndexCustomerViewModel()
+            {
+                Customers = customers
+            };
+
+            return View(indexcustomerviewmodel);
+        }
+
+        [Route("customers/details/{id}")]
+        public ActionResult Details(int id)
+        {
+            //var customer = GetCustomers().SingleOrDefault(c=>c.Id==id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>
+        //    {
+        //        new Customer {Id = 1, Name = "John Smith"},
+        //        new Customer {Id = 2, Name = "Mary Williams"}
+        //    };
+        //}
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        [Route("Movies")]
+        public ViewResult Index()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shrek"},
+                new Movie {Id = 2, Name = "Wall-e"}
+            };
+
+            var indexMovieViewModel = new IndexMovieViewModel()
+            {
+                Movies = movies
+            };
+            return View(indexMovieViewModel);
+        }
+    }
+}
